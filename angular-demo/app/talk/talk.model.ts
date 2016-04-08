795c2870d4022d4f17182506bef6ea7e1d47db2c
Steef Burghouts <Steef@steefs-mbp.domein1.ap>
2016-04-08 09:08:26 +0200

    Added angular demo

---
+++ angular-demo/app/talk/talk.model.ts
@@ -0,0 +1,12 @@
+///<reference path="../venue/venue.model.ts" />
+///<reference path="../timeslot/timeslot.model.ts" />
+
+namespace Talk {
+    export class Talk {
+         public title: string;
+         public key: string;
+         public description: string;
+         public TimeSlotKey: string;
+         public venueKey: string;
+    }
+}
