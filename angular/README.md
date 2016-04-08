3dc03932b3d84009b58052ae44e27e3168b189ec
Frank Folsche <frank.folsche@luminis.eu>
2016-04-08 09:09:48 +0200

    removed old angular demo

--- angular/README.md
+++
@@ -1,9 +0,0 @@
-# Installation
-Prerequisites nodejs
-
-All command are from the commandline with npm installed on the path from the root of this folder
-
-	npm install gulp bower -g
-	npm install
-	bower install
-	tsd install	
--- angular/types/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../typings/jasmine/jasmine.d.ts" />
-/// <reference path="../typings/angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="../typings/angularjs/angular.d.ts" />
-/// <reference path="../typings/jquery/jquery.d.ts" />
-/// <reference path="../typings/angularjs/angular-mocks.d.ts" />
